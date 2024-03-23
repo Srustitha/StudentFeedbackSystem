479afaae034d266f6c9672fe8df0377bc3e6e8e4
Srustitha Simhadri <112477993+Srustitha@users.noreply.github.com>
2024-03-23 12:27:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,32 @@ st.title('STUDENT FEEDBACK ANALYZER')
 csv=st.file_uploader('Enter CSV')
 # Load the dataset
 df = pd.read_csv(csv)
+def generate_summary(teacher_feedback):
+    # Tokenize the feedback into sentences
+    sentences = sent_tokenize(teacher_feedback)
+
+    if len(sentences) == 0:
+        st.text("No feedback available.")
+
+    # Encode sentences into BERT embeddings
+    sentence_embeddings = model.encode(sentences)
+
+    # Calculate the mean embedding of all sentences
+    mean_embedding = sentence_embeddings.mean(axis=0, keepdims=True)
+
+    # Calculate cosine similarity between each sentence embedding and the mean embedding
+    cos_similarities = cosine_similarity(sentence_embeddings, mean_embedding)
+
+    # Sort sentences by cosine similarity in descending order
+    sorted_indices = cos_similarities.flatten().argsort()[::-1]
+
+    # Select the top two sentences as representative
+    num_sentences = min(1,2) # Adjust the number of sentences as needed
+    representative_sentences = [sentences[idx] for idx in sorted_indices[:num_sentences]]
+
+    # Generate summary
+    summary = ' '.join(representative_sentences)
+    return summary
 # Specify the range of teachers to consider
 start_teacher = 1
 end_teacher = 5  # Adjust as needed
